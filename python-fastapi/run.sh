95936d36de0d42a8bd190a7101612898a30b9e20
Marc-Olivier <marc-olivier.bloch@lausanne.ch>
2024-11-23 22:54:47 +0100

    Add gaming perf

--- python-fastapi/run.sh
+++ python-fastapi/run.sh
@@ -8,4 +8,5 @@ fi
 
 . .venv/bin/activate
 pip install -r requirements.txt
-uvicorn app.main:app --reload
+uvicorn src.main:app --workers 5 --log-level critical 
+# For development use --reload instead of '--workers X'
