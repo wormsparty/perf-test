2ac1cf1e36bcf6034188f0e116e7308e371c431a
Marc-Olivier <marc-olivier.bloch@lausanne.ch>
2024-11-23 17:24:17 +0100

    Add results

--- test/run_query.sh
+++ test/run_query.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -H 'Content-Type: application/json' -d @data.json localhost:8000/api/list #> /dev/null 2>&1
+curl -X POST -H 'Content-Type: application/json' -d @data.json localhost:8000/api/list > /dev/null 2>&1
