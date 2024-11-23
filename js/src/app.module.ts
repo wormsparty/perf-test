6fc4eb8b348fdf185662892ab9ac89e96cb36916
Marc-Olivier <marc-olivier.bloch@lausanne.ch>
2024-11-23 16:51:38 +0100

    Use dotenv

--- js/src/app.module.ts
+++ js/src/app.module.ts
@@ -1,19 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntityModule } from './entity/entity.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+console.log(process.cwd());
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'rust',
-      password: 'rust',
-      database: 'rust',
-      entities: ['**/entity/*.entity{.ts,.js}'],
-      synchronize: true,
-      autoLoadEntities: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/../.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_URL'),
+        /*host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),*/
+        entities: ['**/entity/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
     }),
     EntityModule,
   ],
